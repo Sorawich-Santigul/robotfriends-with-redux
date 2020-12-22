50a2c11668517a97a484082f8c75ebf9393e1d8b
unknown <ham_int@hotmail.com>
2020-12-22 17:58:12 +0700

    connecting react-redux

--- src/container/App.js
+++ src/container/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import Cardarray from '../component/Cardarray';
 import SearchBox from '../component/Searchbox';
 import Scroll from '../component/Scroll';
 import './App.css';
 import ErrorBoundry from '../component/ErrorBoundry';
+import {setSearchField} from '../actions.js'
+
+const mapStateToProps=(state)=>{
+  return {
+    searchField: state.searchField,
+  }
+}
+
+const mapDispatchToProps=(dispatch)=>{
+  return{
+    onSearchChange:(event)=>dispatch(setSearchField(event.target.value))
+  }
+}
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
       robots: [],
-      searchfield: ''
     }
   }
 
@@ -19,21 +33,18 @@ class App extends Component {
       .then(users => {this.setState({ robots: users})});
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
-
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots} = this.state;
+    const {searchField,onSearchChange}=this.props
     const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
     return !robots.length ?
       <h1>Loading</h1> :
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <ErrorBoundry>
             <Cardarray robots={filteredRobots} />
@@ -44,4 +55,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './container/App.js';
-ReactDOM.render(<App/>,document.getElementById('root'));
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+import { searchRobot } from './reducers.js'
+import 'tachyons';
+
+const store = createStore(searchRobot);
+
+ReactDOM.render(<Provider store={store}>
+                  <App/>
+                 </Provider>
+	             ,document.getElementById('root'));
+
+
 reportWebVitals();
